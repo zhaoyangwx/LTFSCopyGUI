64ab024ae2a967e1668c9a18638f146216dedde9
zhaoyangwx <zhaoyangwx00@gmail.com>
2024-06-02 16:20:35 +0800

    3.5.3

--- LTFSCopyGUI/My Project/AssemblyInfo.vb
+++ LTFSCopyGUI/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' 方法是按如下所示使用“*”: :
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("3.5.2.3")>
-<Assembly: AssemblyFileVersion("3.5.2.3")>
+<Assembly: AssemblyVersion("3.5.3")>
+<Assembly: AssemblyFileVersion("3.5.3")>
 <Assembly: NeutralResourcesLanguage("")>
