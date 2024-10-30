ff9c50fff0074194a59554e843aecdd31a95e954
zhaoyangwx <zhaoyangwx00@gmail.com>
2024-10-30 18:04:12 +0800

    format dialog fix

--- LTFSCopyGUI/SettingPanel.vb
+++ LTFSCopyGUI/SettingPanel.vb
@@ -31,10 +31,12 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        DialogResult = DialogResult.OK
+        result = DialogResult.OK
+        Close()
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        DialogResult = DialogResult.Cancel
+        result = DialogResult.Cancel
+        Close()
     End Sub
 End Class
