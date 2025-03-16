e1f01de6a94b8d8995bc8dea3246180dbc2401a7
zhaoyangwx <zhaoyangwx00@gmail.com>
2025-03-17 00:48:49 +0800

    SLR -> SLR3

--- LTFSCopyGUI/LTFSWriter.vb
+++ LTFSCopyGUI/LTFSWriter.vb
@@ -3339,7 +3339,7 @@ Public Class LTFSWriter
             While True
                 Dim add_code As UShort = TapeUtils.Locate(driveHandle, schema.previousgenerationlocation.startblock, CByte(schema.previousgenerationlocation.partition), TapeUtils.LocateDestType.Block)
                 p = GetPos
-                If p.PartitionNumber <> CByte(schema.previousgenerationlocation.partition) OrElse p.BlockNumber <> schema.previousgenerationlocation.startblock Then
+                If p.PartitionNumber <> CByte(schema.previousgenerationlocation.partition) OrElse (p.BlockNumber <> schema.previousgenerationlocation.startblock) Then
                     Select Case MessageBox.Show(New Form With {.TopMost = True}, $"Current: P{p.PartitionNumber} B{p.BlockNumber}{vbCrLf}Expected: P{schema.previousgenerationlocation.partition} B{schema.previousgenerationlocation.startblock}{vbCrLf}Additional sense code: 0x{Hex(add_code).ToUpper.PadLeft(4, "0")} {TapeUtils.ParseAdditionalSenseCode(add_code)}", My.Resources.ResText_Warning, MessageBoxButtons.AbortRetryIgnore)
                         Case DialogResult.Ignore
                             Exit While
